5585cbdc42f62d9f879037ef6a5d1c97ed8505cb
FedBashilov <fedbashilov@gmail.com>
2020-02-21 23:55:35 +0300

    finish

--- src/app/pages/task-page/task-page.component.ts
+++ src/app/pages/task-page/task-page.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-
-import { TaskAPIService } from '../../services/task-api.service';
-import { Task } from '../../models/task.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-page',
@@ -10,40 +6,10 @@ import { Task } from '../../models/task.model';
   styleUrls: ['./task-page.component.css']
 })
 export class TaskPageComponent implements OnInit {
-  taskForEdit: Task = new Task;
 
-  constructor(private taskService: TaskAPIService,  private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  addOrEditTask(targetTask: Task){
-    this.router.navigate(['tasks/new']);
-
-
-
-    /*let movingTaskForm: any = document.getElementsByClassName("moving_task_form")[0];
-    let curtain: any = document.getElementsByClassName("curtain")[0];
-
-    if( !movingTaskForm.classList.contains("show") ){
-      movingTaskForm.classList.add("show");
-      curtain.classList.add("show");
-    }
-    console.log(targetTask);
-    if(targetTask){
-      this.taskForEdit = targetTask;
-    } else{
-      this.taskForEdit = new Task;
-    }
-*/
-  }
-
-  hideTaskForm(){
-    let movingTaskForm: any = document.getElementsByClassName("moving_task_form")[0];
-    let curtain: any = document.getElementsByClassName("curtain")[0];
-
-    curtain.classList.remove("show");
-    movingTaskForm.classList.remove("show");
   }
 
 }
